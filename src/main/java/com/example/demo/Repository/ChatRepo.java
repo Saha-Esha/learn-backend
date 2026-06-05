e12f9390e6e3135b27beeb1bd1609a9af1c35e0c
Esha <eshasaha.cse@gmail.com>
2026-06-06 00:18:03 +0600

    Ignore Controller folder

--- src/main/java/com/example/demo/Repository/ChatRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.demo.Entity.ChatMessage;
-
-public interface ChatRepo extends MongoRepository<ChatMessage, String> {}
--- src/main/java/com/example/demo/Repository/LessonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.Entity.Lesson;
-
-public interface LessonRepo extends MongoRepository<Lesson, String> {
-}
